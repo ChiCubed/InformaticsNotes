d8a3703d2e4721b0f495ac94707500d2973af273
ChiCubed <ChiCubed@users.noreply.github.com>
2017-02-22 21:51:23 +0800

    Add handling if pandoc not installed

--- renderpdf.sh
+++ renderpdf.sh
@@ -1,2 +1,7 @@
-pandoc InformaticsNotes.md -s --latex-engine=xelatex \
-       -N --top-level-division=chapter --toc -o InformaticsNotes.pdf
+if ! [ -x "$(command -v pandoc)" ]; then
+  echo 'Error: pandoc not installed. Follow the instructions at http://pandoc.org/installing.html.' >&2
+  exit 1
+fi
+echo 'pandoc installation found... executing command' >&2
+(set -x; pandoc InformaticsNotes.md -s --latex-engine=xelatex \
+       -N --top-level-division=chapter --toc -o InformaticsNotes.pdf)
